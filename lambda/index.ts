6b84f387bbf9bce59cace677947d39d3b4b4b13e
Sasha <sasha.luhr@gmail.com>
2022-03-25 00:52:20 +0100

    wip

---
+++ lambda/index.ts
@@ -0,0 +1,11 @@
+export const handler = async (data: unknown): Promise<unknown> => {
+    //event bridge triggered lambda
+
+    // get api key
+    // make api call to weather api
+    // clean data, create filename
+    // write to s3
+
+    return data;
+
+}
--- lib/aws-weather-app-stack.ts
+++ lib/aws-weather-app-stack.ts
@@ -1,12 +1,14 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
 import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { join } from 'path';
 
-export class AwsWeatherAppStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsWeatherAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
@@ -33,7 +35,7 @@ export class AwsWeatherAppStack extends Stack {
           allowedMethods: [
             s3.HttpMethods.GET,
           ],
-          allowedOrigins: ['http://localhost:3000'],
+          allowedOrigins: ['*'],
           allowedHeaders: ['*'],
         },
       ]
@@ -42,6 +44,15 @@ export class AwsWeatherAppStack extends Stack {
     new s3Deploy.BucketDeployment(this, "StaticPageS3Deploy", {
       sources: [s3Deploy.Source.asset(join(__dirname, "../frontend"))],
       destinationBucket: staticPageS3,
-    })
+    });
+
+    new NodejsFunction(this, "lambda", {
+      description: "Lambda",
+      handler: "handler",
+      entry: join(__dirname, "../lambda/index.ts"),
+      runtime: Runtime.NODEJS_14_X,
+      timeout: cdk.Duration.seconds(30),
+    });
+
   }
 }
