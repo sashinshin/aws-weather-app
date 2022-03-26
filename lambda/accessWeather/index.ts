bfd16a8be71de791dbd4c67a365bc76886729fa8
Sasha <sasha.luhr@gmail.com>
2022-03-26 18:01:53 +0100

    logging

--- lambda/accessWeather/index.ts
+++ lambda/accessWeather/index.ts
@@ -9,6 +9,9 @@ export const handler = async (): Promise<S3.Types.ListObjectsOutput | AWSError> 
         Prefix: "data/stockholm/"
     }
 
+    console.log(getEnvVar("WEATHER_BUCKET_NAME"));
+    
+
     const res =  await new S3().listObjectsV2(param).promise();
     console.log(res);
     
