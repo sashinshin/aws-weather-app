036944f82c21f052843388249dfa9d92d0aecc32
Sasha <sasha.luhr@gmail.com>
2022-03-26 20:03:18 +0100

    accessWeather lambda logic

--- lambda/accessWeather/index.ts
+++ lambda/accessWeather/index.ts
@@ -2,12 +2,28 @@ import { AWSError, S3 } from "aws-sdk";
 import { getEnvVar } from "../../common-utils/index"
 
 
-export const handler = async (): Promise<S3.Types.ListObjectsOutput | AWSError> => {
+export const handler = async (): Promise<S3.GetObjectOutput | AWSError | unknown> => {
 
-    const param = {
+    const listParam = {
         Bucket: getEnvVar("WEATHER_BUCKET_NAME"),
         Prefix: "data/stockholm/"
     }
-    const res =  await new S3().listObjectsV2(param).promise();
+    const s3 = new S3();
+    const objectList =  await s3.listObjectsV2(listParam).promise();
+    const keys = objectList.Contents?.map((obj) => obj.Key ? obj.Key : "");
+    if (!keys) {
+        throw new Error("failed");
+    }
+
+    const promiseList = keys.map((key) => {
+        const param = {
+            Bucket: getEnvVar("WEATHER_BUCKET_NAME"),
+            Key: key,
+        }
+        return s3.getObject(param).promise();
+    });
+    const res = Promise.all(promiseList).then(res => res);
+    console.log(res);
+    
     return res;
 };
